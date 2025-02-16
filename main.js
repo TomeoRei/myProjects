08fa1d79cb49d49cb20a5c0c2cc700b48cfe4908
TomeoRei <tihtimi@gmail.com>
2025-02-16 12:50:49 +0100

    lesson 6
    исправлено гамбургер меню

--- main.js
+++ main.js
@@ -3,11 +3,27 @@ let menu = document.querySelector(".fixed-menu");
 menu.addEventListener('click', function (event) {
     let clickTarget = event.target;
     let activeBtn = document.querySelector('.active');
-
     if (clickTarget.classList.contains('nav-link')) {
         clickTarget.classList.add('active');
         activeBtn.classList.remove('active');
+console.log(5)
+        let mobile = document.querySelector('.mobile-menu');
+        if (!mobile.classList.contains('hide')) {
+            mobile.classList.add('hide');
+        }
+
     }
+
+    let body = document.querySelector("body");
+    let mobileMenu = document.querySelector(".mobile-menu");
+    if (!body.classList.contains("off-scroll") &&
+        !mobileMenu.classList.contains("hide")) {
+        body.classList.add("off-scroll");
+    }
+    else {
+        body.classList.remove("off-scroll");
+    }
+
 });
 
 
@@ -43,27 +59,6 @@ document.querySelector(".mobile-button").addEventListener("click",
         document.querySelector(".mobile-menu").classList.toggle("hide");
     });
 
-menu.addEventListener("click", function (event) {
-    let clickTarget = event.target;
-    let activeBtn = document.querySelector(".active");
-    if (clickTarget.classList.contains("nav-link")) {
-        clickTarget.classList.add("active");
-        activeBtn.classList.remove("active");
 
-        let mobile = document.querySelector(".mobile-menu");
-        if (!mobile.classList.contains("hide")) {
-            mobile.classList.add("hide")
-        }
-    }
-});
 
-let body = document.querySelector("body");
-let mobileMenu = document.querySelector(".mobile-menu");
-if (!body.classList.contains("off-scroll") &&
-    !mobileMenu.classList.contains("hide")) {
-    body.classList.add("off-scroll");
-}
-else {
-    body.classList.remove("off-scroll");
-}
-;
+
